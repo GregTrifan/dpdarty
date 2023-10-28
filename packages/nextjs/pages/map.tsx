71a4ef760ec9ff3e0c00f920af8a2237a7c630fe
Mars Notahacker <marsXrobertson@gmail.com>
2023-10-29 00:58:19 +0100

    Real time Firebase updates

--- packages/nextjs/pages/map.tsx
+++ packages/nextjs/pages/map.tsx
@@ -1,9 +1,9 @@
-/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import { useEffect, useState } from "react";
 import { Party } from "../types/party";
 import { initializeApp } from "firebase/app";
-import { collection, getDocs, getFirestore } from "firebase/firestore";
+import "firebase/firestore";
+import { collection, getFirestore, onSnapshot, query } from "firebase/firestore";
 import MapComponentList from "~~/components/MapComponentList";
 
 const firebaseConfig = {
@@ -15,20 +15,32 @@ const firebaseConfig = {
   appId: "1:969391618248:web:0435b7b6a944abf21dcdfd",
 };
 
-/*const app = */ initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const q = query(collection(db, "parties"));
+
+onSnapshot(q, snapshot => {
+  console.log(snapshot.docs);
+});
 
 const MapPage = () => {
   const [data, setData] = useState<Party[]>([]);
 
   useEffect(() => {
-    async function fetchData() {
-      const db = getFirestore(); // Make sure to initialize your Firestore here
-      const querySnapshot = await getDocs(collection(db, "parties"));
-      const fetchedData = querySnapshot.docs.map(doc => doc.data() as Party);
-      setData(fetchedData);
-    }
+    const unsub = onSnapshot(q, doc => {
+      console.log("test", doc.docs);
 
-    fetchData();
+      //@ts-ignore
+      const dataPartiesArrayFromFirebaseRealTime = [];
+      doc.docs.forEach(doc => {
+        dataPartiesArrayFromFirebaseRealTime.push(doc.data());
+      });
+      //@ts-ignore
+      setData(dataPartiesArrayFromFirebaseRealTime);
+    });
+
+    return () => unsub(); // Clean up the listener on unmount
   }, []); // The empty dependency array means this useEffect runs once when the component mounts
 
   return (
@@ -40,28 +52,8 @@ const MapPage = () => {
       <br />
 
       <MapComponentList addresses={data} />
-
-      {/* {data.map((item, index) => (
-        <MapComponent key={index} address={item.Address} /> // Assuming each item in data has an "address" field
-      ))} */}
     </div>
   );
-
-  // async function fetchData() {
-  //   const querySnapshot = await getDocs(collection(db, "parties"));
-  //   const data = querySnapshot.docs.map(doc => doc.data());
-  //   console.log(data);
-  // }
-
-  // fetchData();
-
-  // return (
-  //   <div className="mx-auto max-w-xl lg:max-w-6xl">
-  //     <h1 className="text-4xl absolute top-4 text-white mr-52">Map</h1>
-  //     <MapComponent address="1600 Amphitheatre Parkway, Mountain View, CA" />
-  //     <MapComponent address="Daszynskiego 19, 31-537 Krakow, Poland" />
-  //   </div>
-  // );
 };
 
 export default MapPage;
