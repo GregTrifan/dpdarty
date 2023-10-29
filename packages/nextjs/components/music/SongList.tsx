ea2ddff34934e2cac1165d7fbf5c2fb95efcfef4
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2023-10-29 09:59:24 +0000

    add voting

--- packages/nextjs/components/music/SongList.tsx
+++ packages/nextjs/components/music/SongList.tsx
@@ -97,92 +97,94 @@ const SongCard = ({ song, songs }: { song: Song; songs: Song[] }) => {
         </div>
         {song.isInQueue && <div className="badge badge-success badge-lg whitespace-nowrap my-auto">IN QUEUE</div>}
       </div>
-      <div className="flex items-right justify-end md:justify-normal w-full gap-1">
-        <button
-          onClick={async () => {
-            if (!isVoted.voted) {
-              const partyRef = doc(db, "parties", id as string);
+      {!song.isInQueue && (
+        <div className="flex items-right justify-end md:justify-normal w-full gap-1">
+          <button
+            onClick={async () => {
+              if (!isVoted.voted) {
+                const partyRef = doc(db, "parties", id as string);
 
-              try {
-                const updatedSongs = songs.map(s => (s.id === song.id ? { ...s, upvotes: s.upvotes + 1 } : s));
+                try {
+                  const updatedSongs = songs.map(s => (s.id === song.id ? { ...s, upvotes: s.upvotes + 1 } : s));
 
-                await updateDoc(partyRef, {
-                  Songs: updatedSongs,
-                });
-                addVotedSong(song.id, "upvote");
-                setIsVoted({ voted: true, action: "upvote" });
-                toast.success("Song upvoted successfully!");
-              } catch (error) {
-                toast.error("Error upvoting song");
-                console.log(error);
+                  await updateDoc(partyRef, {
+                    Songs: updatedSongs,
+                  });
+                  addVotedSong(song.id, "upvote");
+                  setIsVoted({ voted: true, action: "upvote" });
+                  toast.success("Song upvoted successfully!");
+                } catch (error) {
+                  toast.error("Error upvoting song");
+                  console.log(error);
+                }
+              } else {
+                toast.error("You've already voted for this song");
               }
-            } else {
-              toast.error("You've already voted for this song");
-            }
-          }}
-          className={`flex items-center  btn ${
-            isVoted.action === "upvote" ? "btn-success" : "btn-ghost text-success"
-          } rounded-md`}
-        >
-          <ArrowUpIcon className="h-5 w-5" />
-          <span>{song.upvotes}</span>
-        </button>
-        <button
-          onClick={async () => {
-            if (!isVoted.voted) {
-              const partyRef = doc(db, "parties", id as string);
+            }}
+            className={`flex items-center  btn ${
+              isVoted.action === "upvote" ? "btn-success" : "btn-ghost text-success"
+            } rounded-md`}
+          >
+            <ArrowUpIcon className="h-5 w-5" />
+            <span>{song.upvotes}</span>
+          </button>
+          <button
+            onClick={async () => {
+              if (!isVoted.voted) {
+                const partyRef = doc(db, "parties", id as string);
 
-              try {
-                const updatedSongs = songs.map(s => (s.id === song.id ? { ...s, downvotes: s.downvotes + 1 } : s));
+                try {
+                  const updatedSongs = songs.map(s => (s.id === song.id ? { ...s, downvotes: s.downvotes + 1 } : s));
 
-                await updateDoc(partyRef, {
-                  Songs: updatedSongs,
-                });
-                addVotedSong(song.id, "downvote");
-                setIsVoted({ voted: true, action: "downvote" });
-                toast.success("Song upvoted successfully!");
-              } catch (error) {
-                toast.error("Error upvoting song");
-                console.log(error);
+                  await updateDoc(partyRef, {
+                    Songs: updatedSongs,
+                  });
+                  addVotedSong(song.id, "downvote");
+                  setIsVoted({ voted: true, action: "downvote" });
+                  toast.success("Song upvoted successfully!");
+                } catch (error) {
+                  toast.error("Error upvoting song");
+                  console.log(error);
+                }
+              } else {
+                toast.error("You've already voted for this song");
               }
-            } else {
-              toast.error("You've already voted for this song");
-            }
-          }}
-          className={`flex items-center  btn ${
-            isVoted.action === "downvote" ? "btn-error" : "btn-ghost text-error"
-          } rounded-md`}
-        >
-          <ArrowDownIcon className="h-5 w-5" />
-          <span>{song.downvotes}</span>
-        </button>
-        <button
-          onClick={() => {
-            setShowModal(true);
-          }}
-          className="btn btn-secondary flex ml-2 text-black shadow-md rounded-md shadow-secondary/40"
-        >
-          <ChevronDoubleUpIcon className="h-5 w-5" />
-          Super vote
-        </button>
-        {Number(ownedSuperNFTs) > 0 && showModal && (
-          <>
-            <UseSuperVote
-              addToQueue={addSongToQueue}
+            }}
+            className={`flex items-center  btn ${
+              isVoted.action === "downvote" ? "btn-error" : "btn-ghost text-error"
+            } rounded-md`}
+          >
+            <ArrowDownIcon className="h-5 w-5" />
+            <span>{song.downvotes}</span>
+          </button>
+          <button
+            onClick={() => {
+              setShowModal(true);
+            }}
+            className="btn btn-secondary flex ml-2 text-black shadow-md rounded-md shadow-secondary/40"
+          >
+            <ChevronDoubleUpIcon className="h-5 w-5" />
+            Super vote
+          </button>
+          {Number(ownedSuperNFTs) > 0 && showModal && (
+            <>
+              <UseSuperVote
+                addToQueue={addSongToQueue}
+                onClose={() => {
+                  setShowModal(false);
+                }}
+              />
+            </>
+          )}
+          {!ownedSuperNFTs && showModal && (
+            <GetSuperVote
               onClose={() => {
                 setShowModal(false);
               }}
             />
-          </>
-        )}
-        {!ownedSuperNFTs && showModal && (
-          <GetSuperVote
-            onClose={() => {
-              setShowModal(false);
-            }}
-          />
-        )}
-      </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
--- packages/nextjs/components/superVote/UseSuperVote.tsx
+++ packages/nextjs/components/superVote/UseSuperVote.tsx
@@ -1,15 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Modal from "../Modal";
+import { useWalletClient } from "wagmi";
+import { useScaffoldContract, useScaffoldContractRead } from "~~/hooks/scaffold-eth";
 
-const UseSuperVote = ({ onClose }: { onClose: () => void }) => {
+const UseSuperVote = ({ onClose, addToQueue }: { onClose: () => void; addToQueue: () => Promise<void> }) => {
+  const { data: walletClient } = useWalletClient();
+  const { data: ownedSuperNFTs } = useScaffoldContractRead({
+    contractName: "DpDarty",
+    functionName: "getOwnedNFTs",
+    args: [walletClient?.account.address],
+  });
+  const [loading, setIsLoading] = useState(false);
+  const { data: dpdarty } = useScaffoldContract({
+    contractName: "DpDarty",
+    walletClient: walletClient,
+  });
+  const [usesLeft, setUsesLeft] = useState<number>(0);
+  useEffect(() => {
+    const fetchNFTUsages = async () => {
+      if (!dpdarty || !ownedSuperNFTs) return;
+
+      const usagePromises = ownedSuperNFTs.map(async nftId => {
+        const usage = await dpdarty.read.getNFTUsage([nftId]);
+        return 3 - Number(usage);
+      });
+
+      const usages = await Promise.all(usagePromises);
+
+      const totalUsesLeft = usages.reduce((total, uses) => total + uses, 0);
+      setUsesLeft(totalUsesLeft);
+    };
+
+    fetchNFTUsages();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ownedSuperNFTs, loading]);
+
   return (
     <Modal show onClose={onClose}>
-      <h1 className="text-4xl text-white/50 font-bold mb-4">Get SuperVote</h1>
-      <p>
-        Supervotes allows you to directly add your favorite songs in this party&apos;s queue. You can purchase it with
-        20 DPDT Tokens. Each SuperVote NFT can be used up to 3 times.
-      </p>
-      <button className="btn btn-success mt-4">Buy SuperVote NFT</button>
+      <h1 className="text-4xl text-white/50 font-bold mb-4">SuperVote</h1>
+      <p>You&apos;ve got {usesLeft} SuperVotes left</p>
+      <button
+        disabled={loading || !ownedSuperNFTs || ownedSuperNFTs.length === 0}
+        onClick={async () => {
+          setIsLoading(true);
+
+          // Find an NFT with uses left
+          let nftIdToUse = null;
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          for (const nftId of ownedSuperNFTs!) {
+            const usage = await dpdarty?.read.getNFTUsage([nftId]);
+            if (Number(usage) < 3) {
+              nftIdToUse = nftId;
+              break; // Use the first NFT with available uses
+            }
+          }
+
+          if (nftIdToUse !== null) {
+            await dpdarty?.write.useNFTForSuperVote([nftIdToUse]).then(async () => {
+              await addToQueue();
+            });
+          }
+
+          setIsLoading(false);
+          onClose();
+        }}
+        className="btn btn-success mt-4"
+      >
+        {loading && <span className="loading loading-spinner"></span>}
+        Use Supervote
+      </button>
     </Modal>
   );
 };
