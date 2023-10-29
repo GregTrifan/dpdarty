8edd6e170d4cd18b08312671b0e9799ed1d6efea
Mars Notahacker <marsXrobertson@gmail.com>
2023-10-29 02:21:05 +0000

    Moving stuff to "party"

--- packages/nextjs/pages/party/[id].tsx
+++ packages/nextjs/pages/party/[id].tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { collection, onSnapshot, query } from "firebase/firestore";
+import { PlusIcon } from "@heroicons/react/24/outline";
+import { SongList, mockSongs } from "~~/components/music/SongList";
 import { db } from "~~/services/firebase";
 
 const ClubPage = () => {
@@ -24,11 +26,27 @@ const ClubPage = () => {
   }, [id]);
 
   return (
-    <div>
-      <h1 className="text-2xl">{name}</h1>
-      <p>Address: {address}</p>
-      <p className="font-mono">Place ID: {id}</p>
-    </div>
+    <>
+      <div className="mt-8">
+        <h1 className="text-5xl text-center font-black text-secondary">{name}</h1>
+
+        <div className="md:mx-12 max-w-md mx-auto">
+          <h4 className="text-3xl font-bold mt-8">Vote for your favorite song</h4>
+          <SongList songs={mockSongs} />
+          <div className="flex gap-6 justify-start mt-8">
+            <button className="btn btn-circle btn-secondary btn-outline">
+              <PlusIcon />
+            </button>
+            <h5 className="my-auto font-bold text-xl">Add Track</h5>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <p className="font-mono mt-[300px]">Address: {address}</p>
+        <p className="font-mono">Place ID: {id}</p>
+      </div>
+    </>
   );
 };
 
